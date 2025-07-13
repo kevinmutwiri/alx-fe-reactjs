14ac5c028a7b1021c57edb7269584f353916acac
kevinmutwiri <keivnmutwiri@gmail.com>
2025-07-13 11:35:30 +0300

    Keep previous changes so that the checker passes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,26 @@
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <h1>Vite + React</h1>
       <UserProfile
         name="Alice"
         age={25}
         bio="Loves hiking and photography, always seeking new adventures."
       />
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
