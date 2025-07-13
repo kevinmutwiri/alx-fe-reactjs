d407c5c605fc80eb35b1b8597334c9d8c0650d0c
kevinmutwiri <keivnmutwiri@gmail.com>
2025-07-13 11:29:54 +0300

    Create component using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,10 +1,5 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -12,9 +7,11 @@ function App() {
 
   return (
     <>
-      <Header />
-      <MainContent />
-      <Footer />
+      <UserProfile
+        name="Alice"
+        age={25}
+        bio="Loves hiking and photography, always seeking new adventures."
+      />
     </>
   )
 }
